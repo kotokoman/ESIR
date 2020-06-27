2a969524f5a552894179317c357bc2e3fc0930d6
kotokoman <yurikotoman@gmail.com>
2020-06-27 14:29:24 -0300

    Inicial commit final

---
+++ Assets/Scripts/Levels/DarknessMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DarknessMovement : MonoBehaviour
+{
+
+    public float speed = 0.2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(0, transform.position.y - speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
